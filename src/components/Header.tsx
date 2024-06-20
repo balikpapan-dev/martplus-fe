b1b6cdf6f99ffb7b6d1a92dd37049f2053fc6239
Richard Owen <richardowen2411@gmail.com>
2024-06-20 15:01:21 +0800

    init:first initial,first pages, foldering structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { Button } from 'antd';
+
+import logo from '@/assets/logo.jpg';
+
+const Header = () => {
+  return (
+    <header className="fixed w-full bg-white p-3 z-10 shadow-md">
+      <div className="flex justify-between items-center">
+        <Image
+          src={logo}
+          alt="logo"
+          width={40}
+          height={50}
+        />
+        <Button type="primary">
+          Login
+        </Button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
