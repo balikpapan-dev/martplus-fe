6089fd781dfd3856c94fd2cf909d5cf4e61cb6a1
Richard Owen <richardowen2411@gmail.com>
2024-07-12 19:11:33 +0800

    feat:adding bercode component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { Table, Input, Cascader } from "antd";
+import dynamic from "next/dynamic";
 import { FaPlus } from "react-icons/fa6";
 
 import { CITIES } from "@/data/cities";
+
+const BarcodeScanner = dynamic(() => import("@/components/BarcodeScanner"), { ssr: false });
 
 const dataSource = [
   {
@@ -42,7 +45,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col justify-center items-center gap-3">
-        <Input
+        {/* <Input
           addonBefore={<FaPlus
             className="text-lg text-gray-500 cursor-pointer hover:text-gray-700 transition-colors duration-200 ease-in-out"
           />}
@@ -60,7 +63,8 @@ export default function Home() {
           onSearch={() => {}}
           allowClear
           style={{ width: 600 }}
-        />
+        /> */}
+        <BarcodeScanner />
       </div>
       <div>
         <Table
