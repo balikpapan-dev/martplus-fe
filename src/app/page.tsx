8d31a38b628455b4cf1e154bb852df9bd7839cd9
Richard Owen <richardowen2411@gmail.com>
2024-07-17 09:46:25 +0800

    fix:removing ant design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,13 @@
 "use client";
 
-import { Table, Input, Cascader } from "antd";
-import { FaPlus } from "react-icons/fa6";
-
-import { CITIES } from "@/data/cities";
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-];
-
-export default function Home() {
+export default function Page() {
   return (
     <div className="flex flex-col gap-6">
       <div className="flex flex-col justify-center items-center gap-3">
-        <Input
-          addonBefore={<FaPlus
-            className="text-lg text-gray-500 cursor-pointer hover:text-gray-700 transition-colors duration-200 ease-in-out"
-          />}
-          placeholder="Input Harga Barang"
-          allowClear
-          style={{ width: 600 }}
-        />
-        <Input.Search
-          addonBefore={<Cascader
-            placeholder="Kota"
-            options={CITIES}
-            style={{ width: 150 }}
-          />}
-          placeholder="Cari Barang"
-          onSearch={() => {}}
-          allowClear
-          style={{ width: 600 }}
-        />
+        {/* Inputs */}
       </div>
       <div>
-        <Table
-          dataSource={dataSource}
-          columns={columns}
-        />
+        {/* Table */}
       </div>
     </div>
   );
