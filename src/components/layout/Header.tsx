4070c0a4b2dd562dd305cd904295bc2efce3db9b
Richard Owen <richardowen2411@gmail.com>
2024-08-02 10:26:32 +0800

    feat: add button on header to navigate to bulk product page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,23 +7,21 @@ import { FaPlus } from 'react-icons/fa6';
 const Header = () => {
   return (
     <header className="fixed w-full bg-white px-6 py-3 z-10 shadow-md">
-      <Link
-        href="/"
-        className="flex justify-between items-center"
-      >
-        <Image
-          src={logo}
-          alt="logo"
-          width={40}
-          height={50}
-        />
-        {/* Login Button */}
+      <div className="flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="logo"
+            width={40}
+            height={50}
+          />
+        </Link>
         <Link
           href="/product/add"
           className="bg-green-600 p-2 text-white rounded-lg">
           <FaPlus />
         </Link>
-      </Link>
+      </div>
     </header>
   )
 }
